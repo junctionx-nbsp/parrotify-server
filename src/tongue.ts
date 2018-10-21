a5e23d369327ebf3761db9c2bf3e8ff5c8a59d70
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-10-21 02:36:40 +0200

    :bug: add missing uri encoding for lambda handlers

--- src/tongue.ts
+++ src/tongue.ts
@@ -2,11 +2,11 @@ import { get } from "improved/dist/ajax"
 import { TONGUE_BASE_URL } from "./utils/envs"
 
 export async function textToMp3(text: string): Promise<string> {
-  const { url } = await get(`${TONGUE_BASE_URL}/textToMp3?text=${text}`)
+  const { url } = await get(`${TONGUE_BASE_URL}/textToMp3?text=${encodeURIComponent(text)}`)
   return url
 }
 
 export async function mp3ToWav(inputUrl: string): Promise<string> {
-  const { url } = await get(`${TONGUE_BASE_URL}/mp3ToWav?inputUrl=${inputUrl}`)
+  const { url } = await get(`${TONGUE_BASE_URL}/mp3ToWav?inputUrl=${encodeURIComponent(inputUrl)}`)
   return url
 }
