c1d45c50a790811ab4822296bc7dda0d394be9ee
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-10-21 02:24:43 +0200

    :hammer: add more envs and public-ip check

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import "./utils/envs"
 import Server from "./server"
+import { v4 } from "public-ip"
 
-// tslint:disable-next-line:no-magic-numbers
-const s = new Server(80)
-s.run()
+async function run() {
+  process.env.PUBLIC_IP = process.env.PUBLIC_IP || await v4()
+
+  const s = new Server(80)
+  s.run()
+}
+
+run()
--- src/utils/envs.ts
+++ src/utils/envs.ts
@@ -2,17 +2,29 @@ import { config } from "dotenv"
 config()
 
 const {
-  TONGUE_BASE_URL = ""
+  TONGUE_BASE_URL = "",
+  SUBSCRIPTION_ID = "",
+  NOKIA_BASE_URL = "",
+  NOKIA_API_KEY = "",
+  FALLBACK_AUDIO_URL = ""
 } = process.env
 
 if (
   [
-    TONGUE_BASE_URL
+    TONGUE_BASE_URL,
+    SUBSCRIPTION_ID,
+    NOKIA_BASE_URL,
+    NOKIA_API_KEY,
+    FALLBACK_AUDIO_URL
   ].some(a => a === "")
 ) {
   throw new Error(`Envs not set properly: ${JSON.stringify(process.env)}`)
 }
 
 export {
-  TONGUE_BASE_URL
+  TONGUE_BASE_URL,
+  SUBSCRIPTION_ID,
+  NOKIA_BASE_URL,
+  NOKIA_API_KEY,
+  FALLBACK_AUDIO_URL
 }
