2ef8974c3871dc69819721039cde265a055b4776
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-10-21 02:35:01 +0200

    :rocket: extend API capabilities

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,43 @@
 import { CallAction } from "./types"
+import { SUBSCRIPTION_ID, NOKIA_BASE_URL, NOKIA_API_KEY, FALLBACK_AUDIO_URL } from "./utils/envs"
+import { post, del } from "improved/dist/ajax"
 
+let lastKnownAudioFile = FALLBACK_AUDIO_URL
+
+export async function unsubscribe(displayAddress: string) {
+  return del(`${NOKIA_BASE_URL}/subscriptions/callDirection/subs?Id=${encodeURIComponent(SUBSCRIPTION_ID)}&addr=${encodeURIComponent(displayAddress)}`, { authorization: NOKIA_API_KEY })
+}
+
+export async function subscribe(displayAddress: string) {
+  const body = {
+    callDirectionSubscription: {
+      callbackReference: {
+        notifyURL: `${process.env.PUBLIC_IP}/callevent`
+      },
+      filter: {
+        address: [
+          displayAddress
+        ],
+        criteria: [
+          "CalledNumber",
+          // "Busy",
+          // "Disconnected"
+          "Answer",
+        ],
+        addressDirection: "Called"
+      },
+      clientCorrelator: SUBSCRIPTION_ID
+    }
+  }
+  return post(`${NOKIA_BASE_URL}/subscriptions/callDirection/subs?Id=${encodeURIComponent(SUBSCRIPTION_ID)}&addr=${encodeURIComponent(displayAddress)}`, body, false, { authorization: NOKIA_API_KEY })
+}
+
+export function handleCall(displayAddress: string, actionToPerform: CallAction.PromptInput, secondAddress: string, audioFileUrl?: string): {}
 export function handleCall(displayAddress: string, actionToPerform: CallAction.Continue | CallAction.EndCall): {}
-export function handleCall(displayAddress: string, actionToPerform: CallAction.PlayAudio, routingAddress: undefined, audioFileUrl: string): {}
-export function handleCall(displayAddress: string, actionToPerform: CallAction.Route, routingAddress: string): {}
-export function handleCall(displayAddress: string, actionToPerform: CallAction, routingAddress?: string, audioFileUrl?: string): {} {
+export function handleCall(displayAddress: string, actionToPerform: CallAction.PlayAudio, secondAddress: undefined, audioFileUrl: string): {}
+export function handleCall(displayAddress: string, actionToPerform: CallAction.Route, secondAddress: string): {}
+export function handleCall(displayAddress: string, actionToPerform: CallAction, secondAddress?: string, audioFileUrl?: string): {} {
+  if (audioFileUrl) lastKnownAudioFile = audioFileUrl
   switch (actionToPerform) {
     case CallAction.Continue:
     case CallAction.EndCall:
@@ -17,7 +51,7 @@ export function handleCall(displayAddress: string, actionToPerform: CallAction, 
       return {
         action: {
           actionToPerform: "Route",
-          routingAddress,
+          secondAddress,
           displayAddress
         }
       }
@@ -28,7 +62,7 @@ export function handleCall(displayAddress: string, actionToPerform: CallAction, 
           displayAddress,
           digitCapture: {
             playingConfiguration: {
-              playFileLocation: audioFileUrl
+              playFileLocation: audioFileUrl || lastKnownAudioFile
             },
             callParticipant: [
               displayAddress
@@ -36,32 +70,30 @@ export function handleCall(displayAddress: string, actionToPerform: CallAction, 
           }
         }
       }
+    case CallAction.PromptInput:
+      return {
+        action: {
+          actionToPerform: "Continue",
+          displayAddress,
+          digitCapture: {
+            digitConfiguration: {
+              maxDigits: 100,
+              minDigits: 1,
+              endChar: "#"
+            },
+            playingConfiguration: {
+              playFileLocation: audioFileUrl || lastKnownAudioFile
+            },
+            callParticipant: [
+              secondAddress
+            ]
+          },
+          playAndCollectInteractionSubscription: {
+            callbackReference: {
+              notifyURL: `${process.env.PUBLIC_IP}/callevent`
+            }
+          }
+        }
+      }
   }
 }
-
-/*
-const DIGIT_CAPTURED = (displayAddress: string, audioFileUrl: string, playAndCollectCallbackUrl: string) => ({
-  action: {
-    actionToPerform: "Continue",
-    displayAddress,
-    digitCapture: {
-      digitConfiguration: {
-        maxDigits: 10,
-        minDigits: 3,
-        endChar: "#"
-      },
-      playingConfiguration: {
-        playFileLocation: audioFileUrl
-      },
-      callParticipant: [
-        displayAddress
-      ]
-    },
-    playAndCollectInteractionSubscription: {
-      callbackReference: {
-        notifyURL: playAndCollectCallbackUrl
-      }
-    }
-  }
-})
-*/
